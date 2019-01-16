c935fb43dfbe448715aced62d8a7c9cb01f41568
woraput <woraput@kkumail.com>
2019-01-16 10:21:28 +0700

    setup project q1 q2

---
+++ Q1/q1Classlib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace q1Classlib
+{
+    public class Class1
+    {
+    }
+}
---
+++ Q1/q1Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace q1Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
